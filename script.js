ee1b386985af2e5ba81c90c4e4969e5f0346211b
Abhiram Girish Naik <149358568+abhiramnaik33@users.noreply.github.com>
2025-03-25 10:55:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nav = document.querySelector('nav');
+    
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+            nav.classList.add('scrolled');
+        } else {
+            nav.classList.remove('scrolled');
+        }
+    });
+});
+
